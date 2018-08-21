cbcf7ee9c3821447a6f6dc041283735fe23d455f
clairemacdonald <claire.macdonald@blis.com>
2018-08-21 19:36:47 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
+document.addEventListener("DOMContentLoaded" , e => {
+  
+  document.querySelector("body")
+  //.addEventListener("click", e => console.log("Reggae, Reggae!"));
+  
+  .addEventListener("click" , e =>console.log("Reggae, Reggae!"));
+  
+})
