d62814e19159df5033e6c9e1536d28ef7b08aee0
clairemacdonald <claire.macdonald@blis.com>
2018-08-21 19:43:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 /* code goes here */
+const expect = chai.expect;
+
+describe('index.js', function () {
+  it('must locate div#hidden-div and update its innerText from 300 to 301', function () {
+    expect(document.querySelector("div#hidden-div").innerText).to.equal('301');
+  });
+});
